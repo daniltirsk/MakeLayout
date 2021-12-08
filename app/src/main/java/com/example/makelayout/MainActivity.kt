90de3fd09c5d0025aba470b747f3503af58e20d1
Ivan Petr <ivan.kiel@gmail.com>
2021-12-08 13:18:47 +0800

    распределение картинок

--- app/src/main/java/com/example/makelayout/MainActivity.kt
+++ app/src/main/java/com/example/makelayout/MainActivity.kt
@@ -13,11 +13,24 @@ import android.widget.LinearLayout
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val layout = LinearLayout(applicationContext)
+        layout.orientation = LinearLayout.VERTICAL
+
         val params = LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT)
         params.weight = 1.toFloat() // единичный вес
 
-        val cat = ImageView(applicationContext)
-        cat.setImageResource(R.drawable.squarecat); cat.layoutParams = params
+        val catViews = ArrayList<ImageView>()
+        for (i in 1..16) {
+            catViews.add( // вызываем конструктор для создания нового ImageView
+                    ImageView(applicationContext).apply {
+                        setImageResource(R.drawable.squarecat)
+                        layoutParams = params
+                    })
+        }
+
+        val rows = ArrayList<LinearLayout>()
+        // TODO: 1) заполнить 4 строки элементами из массива catViews по 4 штуки в ряду
+ /*
         val cat2 = ImageView(applicationContext)
         cat2.setImageResource(R.drawable.squarecat); cat2.layoutParams = params
         val cat3 = ImageView(applicationContext)
@@ -26,8 +39,7 @@ class MainActivity : AppCompatActivity() {
         //cat.layoutParams = ViewGroup.LayoutParams(applicationContext, )
         cat4.setImageResource(R.drawable.squarecat)
 
-        val layout = LinearLayout(applicationContext)
-        layout.orientation = LinearLayout.VERTICAL
+
 
         val row1 = LinearLayout(applicationContext)
         row1.orientation = LinearLayout.HORIZONTAL
@@ -40,6 +52,9 @@ class MainActivity : AppCompatActivity() {
         row2.addView(cat3); row2.addView(cat4);
 
         layout.addView(row1); layout.addView(row2)
+
+
+  */
         setContentView(layout)
         //setContentView(R.layout.activity_main)
     }
