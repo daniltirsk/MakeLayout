24361230724ea10006e807d7e78bcb65f03e160e
danil <daniltirskk@gmail.com>
2021-12-23 14:17:36 +0800

    initial commit

--- app/src/main/java/com/example/makelayout/MainActivity.kt
+++ app/src/main/java/com/example/makelayout/MainActivity.kt
@@ -5,14 +5,17 @@ import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.AttributeSet
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.Toast
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,70 +23,89 @@ class MainActivity : AppCompatActivity() {
         val layout = LinearLayout(applicationContext)
         layout.orientation = LinearLayout.VERTICAL
 
-        val params = LinearLayout.LayoutParams(0, LinearLayout.LayoutParams.WRAP_CONTENT)
-        params.weight = 1.toFloat() // единичный вес
+        val params = LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.MATCH_PARENT)
+        params.weight = 1.toFloat()
 
-        // TODO: 3) реализовать переворот карт с "рубашки" на лицевую сторону и обратно
-        val colorListener = View.OnClickListener() {
-            GlobalScope.launch (Dispatchers.Main)
-                { setBackgroundWithDelay(it) }
-            //it.setBackgroundColor(Color.YELLOW)
+        val fronts = arrayOf(R.drawable.bee,R.drawable.butterfly,R.drawable.butterfly_1,R.drawable.butterfly_2,
+            R.drawable.chick, R.drawable.flower, R.drawable.ladybug, R.drawable.sunflower
+        )
 
-        }
-
+        var firstCard: View = View(this)
+        var openCardsCount = 0 // число открытых карт
+        var guessedRightCount = 0
 
         val catViews = ArrayList<ImageView>()
+        fronts.shuffle()
 
-        // TODO: 2) случайным образом разместить 8 пар картинок
+        val colorListener = View.OnClickListener() {
+            when (openCardsCount) {
+                0 -> {
+                    firstCard = it;
+                    openCardsCount++;
+                    GlobalScope.launch (Dispatchers.Main) { flipOver(firstCard as ImageView,fronts[it.tag.toString().toInt()]) }
+                }
+                1 -> {
+                    if (it.tag == firstCard.tag) {
+                        GlobalScope.launch (Dispatchers.Main) {
+                            openCardsCount++;
+                            flipOver(it as ImageView,fronts[it.tag.toString().toInt()])
+                            delay(1000)
+                            hide(it as ImageView)
+                            hide(firstCard as ImageView)
+                            openCardsCount = 0
+                            guessedRightCount += 1
+                            if (guessedRightCount == fronts.size){
+                                val text = "You've Won!"
+                                val duration = Toast.LENGTH_SHORT
+                                val toast = Toast.makeText(applicationContext, text, duration)
+                                toast.show()
+                            }
+                        }
+                    } else {
+                        GlobalScope.launch (Dispatchers.Main) {
+                            openCardsCount++;
+                            flipOver(it as ImageView,fronts[it.tag.toString().toInt()])
+                            delay(1000)
+                            flipOver(firstCard as ImageView,R.drawable.back)
+                            flipOver(it as ImageView, R.drawable.back)
+                            openCardsCount = 0
+                        }
+                    }
+                }
+                else -> Log.d("mytag", "two cards are open already") // ничего не делаем
+            }
+        }
 
         for (i in 1..16) {
             catViews.add( // вызываем конструктор для создания нового ImageView
                     ImageView(applicationContext).apply {
-                        setImageResource(R.drawable.squarecat)
+                        setImageResource(R.drawable.back)
+                        tag = (i-1)/2
                         layoutParams = params
                         setOnClickListener(colorListener)
-
                     })
         }
+        catViews.shuffle()
+
         val rows = Array(4, { LinearLayout(applicationContext)})
         var count = 0
+
         for (view in catViews) {
             val row: Int = count / 4
             rows[row].addView(view)
             count ++
         }
+
         for (row in rows) {
-            layout.addView(row)
+            row.apply{
+                orientation = LinearLayout.HORIZONTAL
+                layoutParams = params
+            }
+                layout.addView(row)
         }
 
-        // TODO: 1) заполнить 4 строки элементами из массива catViews по 4 штуки в ряду
- /*
-        val cat2 = ImageView(applicationContext)
-        cat2.setImageResource(R.drawable.squarecat); cat2.layoutParams = params
-        val cat3 = ImageView(applicationContext)
-        cat3.setImageResource(R.drawable.squarecat)
-        val cat4 = ImageView(applicationContext)
-        //cat.layoutParams = ViewGroup.LayoutParams(applicationContext, )
-        cat4.setImageResource(R.drawable.squarecat)
-
-
-
-        val row1 = LinearLayout(applicationContext)
-        row1.orientation = LinearLayout.HORIZONTAL
-        row1.setBackgroundColor(Color.GRAY)
-        row1.addView(cat2); row1.addView(cat);
-
-        val row2 = LinearLayout(applicationContext)
-        row2.orientation = LinearLayout.HORIZONTAL
-        row2.setBackgroundColor(Color.GRAY)
-        row2.addView(cat3); row2.addView(cat4);
-
-        layout.addView(row1); layout.addView(row2)
-
-
-  */
         setContentView(layout)
-        //setContentView(R.layout.activity_main)
     }
 
     suspend fun setBackgroundWithDelay(v: View) {
@@ -94,7 +116,14 @@ class MainActivity : AppCompatActivity() {
         v.isClickable = false
     }
 
-    suspend fun openCards() {
 
+    suspend fun flipOver(v: ImageView,r: Int) {
+        v.setImageResource(r)
+        v.isClickable = !v.isClickable
+    }
+
+    suspend fun hide(v: ImageView) {
+        v.visibility = View.INVISIBLE
+        v.isClickable = false
     }
 }
